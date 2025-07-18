ffbc60decbb8aaecbc14172c311716b497ace0a2
sleepleech <faqihwhy119@gmail.com>
2025-07-18 19:53:04 +0700

    __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,10 +1,10 @@
 import asyncio
 from bot.core.mltb_client import TgClient
-import bot.handlers  # Pastikan folder handlers memiliki file _init_.py atau handler.py
+import bot.handlers  # Pastikan file handler-nya benar
 
 async def main():
     print("Menyalakan Sleepy-LeechBot...")
     await TgClient.start_bot()
 
-if __name__ == "_main_":
+if __name__ == "__main__":
     asyncio.run(main())
