601900e2f3524d9a0e9723cd21661836cf03e4c5
sleepleech <faqihwhy119@gmail.com>
2025-07-18 20:05:26 +0700

    start_handler.py

--- bot/handlers/start_handler.py
+++ bot/handlers/start_handler.py
@@ -1,6 +1,7 @@
-from pyrogram import Client, filters
+from pyrogram import filters
 from pyrogram.types import Message
+from bot.core.client_instance import bot
 
-@Client.on_message(filters.command("start") & filters.private)
-async def start_command(client: Client, message: Message):
+@bot.on_message(filters.command("start") & filters.private)
+async def start_command(client, message: Message):
     await message.reply_text("Halo! Bot aktif 👋")
