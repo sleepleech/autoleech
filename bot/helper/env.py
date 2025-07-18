02792070cf2854fd56f9ee14aafe7ba3b932237a
sleepleech <faqihwhy119@gmail.com>
2025-07-18 16:31:57 +0700

    env.py

---
+++ bot/helper/env.py
@@ -0,0 +1,9 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+OWNER_ID = int(os.getenv("OWNER_ID"))
