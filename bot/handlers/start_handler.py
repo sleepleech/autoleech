863a2e3eee5ab363857d8b1281013870fda79116
sleepleech <faqihwhy119@gmail.com>
2025-07-18 19:14:57 +0700

    start_handler.py

--- bot/handlers/start_handler.py
+++ bot/handlers/start_handler.py
@@ -3,7 +3,4 @@ from pyrogram.types import Message
 
 @Client.on_message(filters.command("start") & filters.private)
 async def start_command(client: Client, message: Message):
-    await message.reply_text(
-        f"Halo {message.from_user.first_name}! 👋\n\n"
-        "Ini adalah Sleepy-LeechBot, siap membantumu!"
-    )
+    await message.reply_text("Halo! Bot aktif 👋")
