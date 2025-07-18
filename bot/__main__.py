d5f8ecfecf275c3c970a7b6393feccb9f8a85904
sleepleech <faqihwhy119@gmail.com>
2025-07-18 18:55:40 +0700

    __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,10 +1,10 @@
 import asyncio
+from pyrogram import idle
 from bot.core.mltb_client import TgClient
 
 async def main():
-    await asyncio.gather(
-        TgClient.start_bot()
-    )
+    await TgClient.start_bot()
+    await idle()  # biar bot standby menunggu pesan masuk
 
 if __name__ == "__main__":
     asyncio.run(main())
